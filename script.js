1b865c401fc8582486a0f60d2e36bbaa6c115a9f
mechac <71565445+mechac@users.noreply.github.com>
2025-03-07 19:12:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
- const spinner = document.getElementById('spinner');
- 
- // Удаляем анимацию через 3 секунды
- setTimeout(() => {
- spinner.style.animation = 'none';
- spinner.style.display = 'none'; // Скрываем изображение после анимации
- }, 1500);
-});
+
